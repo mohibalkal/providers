9879a0cfc408415e0e543f27c11181d6ec7b8d09
mohibalkal <mohibkal1978@gmail.com>
2026-08-16 17:26:22 +0200

    chore: remove all fontaine.lol scrapers and their embeds

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -1,25 +1,5 @@
 // import { artemisScraper } from './sources/artemis';
-import { asperaScraper } from './sources/aspera';
-// import { tikkiScraper } from './sources/tikki';
-import { veloraScraper } from './sources/velora';
-import { vaultScraper } from './sources/vault';
-import { oregonScraper } from './sources/oregon';
-// import { tokyoScraper } from './sources/tokyo';
-import { chaseScraper } from './sources/chase';
-import { tikkiScraper } from './sources/tikki';
-import { valleyScraper } from './sources/valley';
-import { sidoniaScraper } from './sources/sidonia';
-import { nesterovScraper } from './sources/nesterov';
-import { viennaScraper } from './sources/vienna';
-import { nekoScraper } from './sources/neko';
-import { oregonNorthwestEmbed, oregonCorduroyEmbed, oregonPinesEmbed, oregonGleefulEmbed } from './embeds/oregon';
-import { chaseSaturnEmbed, chaseMercuryEmbed, chaseMarsEmbed, chaseVenusEmbed } from './embeds/chase';
-import { tikkiNovaEmbed, tikkiAtlasEmbed, tikkiOrionEmbed } from './embeds/tikki';
-import { valleyAbigailEmbed, valleyLinusEmbed, valleyPennyEmbed, valleySebastianEmbed, valleyHarveyEmbed } from './embeds/valley';
-import { sidoniaTheiaEmbed, sidoniaCriusEmbed, sidoniaHadesEmbed } from './embeds/sidonia';
 import { warezcdnembedmp4Scraper } from './embeds/warezcdnembedmp4';
-import { veloraYoruEmbed, veloraNeonEmbed } from './embeds/velora';
-import { vaultQuartzEmbed, vaultAndesiteEmbed } from './embeds/vault';
 
 import { Sourcerer, Embed } from './base';
 import { serverMirrorEmbed } from './embeds/server-mirrors';
@@ -38,19 +18,6 @@ import { debridScraper } from './sources/debrid';
 
 export function gatherAllSources(): Array<Sourcerer> {
   return [
-    nekoScraper,
-    viennaScraper,
-    sidoniaScraper,
-    valleyScraper,
-    chaseScraper,
-    tikkiScraper,
-    nesterovScraper,
-    // tokyoScraper,
-    oregonScraper,
-    vaultScraper,
-    veloraScraper,
-    // tikkiScraper,
-    asperaScraper,
     cinevaroScraper,
 
     gallicScraper,
@@ -76,29 +43,6 @@ export function gatherAllEmbeds(): Array<Embed> {
     warezcdnembedmp4Scraper,
     serverMirrorEmbed,
     turbovidScraper,
-    vaultQuartzEmbed,
-    vaultAndesiteEmbed,
-    veloraYoruEmbed,
-    veloraNeonEmbed,
-    oregonNorthwestEmbed,
-    oregonCorduroyEmbed,
-    oregonPinesEmbed,
-    oregonGleefulEmbed,
-    chaseSaturnEmbed,
-    chaseMercuryEmbed,
-    chaseMarsEmbed,
-    chaseVenusEmbed,
-    tikkiNovaEmbed,
-    tikkiAtlasEmbed,
-    tikkiOrionEmbed,
-    valleyAbigailEmbed,
-    valleyLinusEmbed,
-    valleyPennyEmbed,
-    valleySebastianEmbed,
-    valleyHarveyEmbed,
-    sidoniaTheiaEmbed,
-    sidoniaCriusEmbed,
-    sidoniaHadesEmbed,
     ...gallicEmbeds,
   ];
 }
